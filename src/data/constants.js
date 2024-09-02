27a7c22464b7dc815732bc2f66aa76fa55290c22
Bhaskar <bhaskaryvora@gmail.com>
2024-09-02 15:58:32 -0400

    File changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/bhaskarvora",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1zQPDZ7VAbXpokC5NfnEK0HepB8SK3qWY/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/bhaskar-vora-byv/",
   X: "https://x.com/bhaskarvora",
   insta: "https://www.instagram.com/bhaskaryvora/",
